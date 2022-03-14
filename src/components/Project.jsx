3db2cbd88549ad0e242f33ad5fde92101bc5071f
Adam <adampugh@zoho.com>
2022-03-14 23:58:38 +0000

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,29 +1,26 @@
 import RotatingText from './RotatingText';
-import ExampleImage from '../assets/images/example.png';
+import ExampleImage from '../assets/images/example2.jpg';
 
 import '../styles/components/Project.scss';
 
-const Project = () => {
-    return (
-        <div className='project container'>
-            <div className='image__wrapper'>
-                <p className='image__text spaced-text'>PrepXP</p>
-                <img src={ExampleImage} alt='prepxp' />
-            </div>
-            <div className='project__text'>
-                <p className='project__text__tech spaced-text'>React | Node | TypeScript</p>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae accusantium, numquam non, optio est
-                    nobis, odit incidunt adipisci consequuntur cumque sequi rem nihil saepe earum obcaecati eius libero
-                    explicabo magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
-                    explicabo?
-                </p>
-                <div className='project__rotating__text spaced-text'>
-                    <RotatingText text='View ☠ Project ☠ ' />
-                </div>
+const Project = () => (
+    <div className='project container'>
+        <div className='image__wrapper'>
+            <p className='image__text spaced-text'>PrepXP</p>
+            <img src={ExampleImage} alt='prepxp' />
+        </div>
+        <div className='project__text'>
+            <p className='project__text__tech spaced-text'>React | Redux | Firebase</p>
+            <p>
+                PrepXP is a web application built in React, Redux and Firebase that allows users to create and search
+                for lists of job interview questions. PrepXP features a blog section created in Node and a comprehensive
+                test suite built in Jest and Enzyme.
+            </p>
+            <div className='project__rotating__text spaced-text'>
+                <RotatingText text='View ☠ Project ☠ ' />
             </div>
         </div>
-    );
-};
+    </div>
+);
 
 export default Project;
