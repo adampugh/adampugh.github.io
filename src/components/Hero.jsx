59440137e76afb4ea3c535cd34fafa33466d24a3
Adam <adampugh@zoho.com>
2022-04-04 20:32:50 +0100

    responsive refactor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,57 @@
+import useWindowDimensions from '../hooks/useWindowDimensions';
 import '../styles/components/Hero.scss';
 
 import FadeIn from './FadeIn';
 import StaggeredText from './StaggeredText';
 import RotatingText from './RotatingText';
 
+const XsScreenStaggeredText = () => <></>;
+
+const SScreenStaggeredText = () => (
+    <>
+        <h1 className='hero__title'>
+            <StaggeredText text='Fron' />
+        </h1>
+        <h1 className='hero__title hero__title--second'>
+            <StaggeredText text='end' />
+        </h1>
+        <h1 className='hero__title hero__title--second'>
+            <StaggeredText text='Dev' />
+        </h1>
+        <h1 className='hero__title hero__title--second'>
+            <StaggeredText text='elop' />
+        </h1>
+        <h1 className='hero__title hero__title--second'>
+            <StaggeredText text='er' />
+        </h1>
+    </>
+);
+
+const LgScreenStaggeredText = () => (
+    <>
+        <h1 className='hero__title'>
+            <StaggeredText text='Frontend' />
+        </h1>
+        <h1 className='hero__title hero__title--second'>
+            <StaggeredText text='Developer' />
+        </h1>
+    </>
+);
+
 const Hero = () => {
+    const { width } = useWindowDimensions();
+
     return (
         <section className='container' id='hero'>
             <div className='hero__title__container'>
                 <div className='hero__title__wrapper'>
-                    <h1 className='hero__title'>
-                        <StaggeredText text='Frontend' />
-                    </h1>
-                    <h1 className='hero__title hero__title--second'>
-                        <StaggeredText text='Developer' />
-                    </h1>
+                    {width < 300 ? (
+                        <XsScreenStaggeredText />
+                    ) : width < 920 ? (
+                        <SScreenStaggeredText />
+                    ) : (
+                        <LgScreenStaggeredText />
+                    )}
                 </div>
             </div>
             <FadeIn>
