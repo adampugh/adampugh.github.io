41b946d32eb769a40f166057f5d5303f603cf778
adampugh <adampugh@zoho.com>
2026-08-01 04:21:30 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.scss'
+
+const App = () => {
+  return <h1 className="card">hello</h1>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: { '@': '/src' },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/mixins" as m;`,
+      },
+    },
+  },
+})
